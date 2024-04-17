a04f5e95e65c82190aee5d82f2a5d67e2f880050
DianaDzakah <dianadzakah1@gmail.com>
2024-04-17 02:05:27 +0000

    Co-authored-by: mickeymond@gmail.com <mickeymond@gmail.com>

---
+++ frontend/src/components/add-event/index.jsx
@@ -0,0 +1,7 @@
+export default function AddEvent() {
+	return (
+		<>
+			<p>Add Event Dialog goes here</p>
+		</>
+	);
+}
--- frontend/src/pages/calendar/index.jsx
+++ frontend/src/pages/calendar/index.jsx
@@ -1,28 +1,57 @@
-import React from 'react';
-import { Calendar, momentLocalizer } from 'react-big-calendar';
-import moment from 'moment';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
+import React from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import Modal from "react-modal";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 import styles from "./index.module.css"; // Import CSS module
+import AddEvent from "../../components/add-event";
 
 const localizer = momentLocalizer(moment);
 
-const MyCalendar = ({ events }) => (
-  <>
-    <nav className={styles.nav}> {/* Apply CSS styles */}
-      <a href='/calendar' className={styles.planit}>planit</a>
-      <a className={styles.link} href='/login'>LogOut</a>
-    </nav>
+const MyCalendar = ({ events }) => {
+	const [modalIsOpen, setIsOpen] = React.useState(true);
 
-    <div  style={{ height: '800px' }}>
-      <Calendar
-        localizer={localizer}
-        events={events}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ margin: '50px' }}
-      />
-    </div>
-  </>
-);
+	return (
+		<>
+			<nav className={styles.nav}>
+				{" "}
+				{/* Apply CSS styles */}
+				<a href="/calendar" className={styles.planit}>
+					planit
+				</a>
+				<a className={styles.link} href="/login">
+					LogOut
+				</a>
+			</nav>
+
+			<Modal
+				style={{
+					content: {
+						width: "400px",
+						height: "400px",
+						top: "20%",
+						left: "20%",
+					},
+					overlay: {
+						opacity: 1,
+					},
+				}}
+				isOpen={modalIsOpen}
+			>
+				<AddEvent />
+			</Modal>
+
+			<div style={{ height: "800px" }}>
+				<Calendar
+					localizer={localizer}
+					events={events}
+					startAccessor="start"
+					endAccessor="end"
+					style={{ margin: "50px" }}
+				/>
+			</div>
+		</>
+	);
+};
 
 export default MyCalendar;
