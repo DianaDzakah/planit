c56c573ac500761872534ef466387a3cca153f1b
DianaDzakah <dianadzakah1@gmail.com>
2024-04-18 18:00:54 +0000

    changes

--- frontend/src/components/add-event/index.jsx
+++ frontend/src/components/add-event/index.jsx
@@ -1,7 +1,22 @@
-export default function AddEvent() {
+import React from "react";
+import styles from "./index.module.css";
+
+const AddEvent = () => {
 	return (
 		<>
-			<p>Add Event Dialog goes here</p>
+			<div className={styles.card}>
+				<div className={styles.cardContent}>
+					<h2 className={styles.eventTitle}>Event Title</h2>
+					<p className={styles.eventtDate}>Date: January 1, 2025</p>
+					<p className={styles.eventDate}>end date</p>
+				</div>
+				<div className={styles.cardAction}>
+					<button className={styles.actionButton}>Edit</button>
+					<button className={styles.actionButton}>Delete</button>
+				</div>
+			</div>
 		</>
 	);
-}
+};
+
+export default AddEvent;
