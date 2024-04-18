79178cded9f359b3a0cb500d37aef8ee23ec5e1e
DianaDzakah <dianadzakah1@gmail.com>
2024-04-18 11:31:12 +0000

    changes

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,13 @@
+const registerNewUser = (req, res) => {
+	console.log("register a new user");
+};
+
+const loginUser = (req, res) => {
+	console.log("log in user");
+};
+
+const displayUserProfile = (req, res) => {
+	console.log('display user profile');
+};
+
+export { registerNewUser, loginUser, displayUserProfile};
