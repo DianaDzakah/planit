820974a0f2111109f4758aab214af3cc099940f6
DianaDzakah <dianadzakah1@gmail.com>
2024-04-20 13:22:28 +0000

    updated user schema

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,5 +1,6 @@
 // Import mongoose
 import mongoose from "mongoose";
+import validator from "validator";
 
 // Destructure mongoose objects
 const { Schema, model } = mongoose;
@@ -9,18 +10,36 @@ const userSchema = new Schema({
 	firstName: {
 		type: String,
 		required: true,
+		trim: true,
 	},
 	lastName: {
 		type: String,
 		required: true,
+		trim: true,
 	},
 	email: {
 		type: String,
 		required: true,
+		unique: true,
+		lowercase: true,
+		trim: true,
+		validate(value) {
+			if (!validator.isEmail(value)) {
+				throw new Error("email is invalid");
+			}
+		},
 	},
 	password: {
 		type: String,
 		required: true,
+		minLength: 6,
+		validate(value) {
+			if (value.toLowerCase().includes("password")) {
+				throw new Error(
+					"invalid password, password cannot contain the word 'password'"
+				);
+			}
+		},
 	},
 });
 
